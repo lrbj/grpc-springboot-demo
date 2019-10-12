29698d676c6364c86518aa186a39c161a9df3e21
H299633 <shuzhen.ye@honeywell.com>
2019-10-12 17:05:37 +0800

    first commit

---
+++ grpc-client/src/main/java/com/lrbj/grpcclient/GrpcClientService.java
@@ -0,0 +1,23 @@
+package com.lrbj.grpcclient;
+
+
+import com.lrbj.grpc.lib.GreeterGrpc;
+import com.lrbj.grpc.lib.GreeterOuterClass;
+import io.grpc.Channel;
+
+import net.devh.springboot.autoconfigure.grpc.client.GrpcClient;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GrpcClientService {
+
+    @GrpcClient("local-grpc-server")
+    private Channel serverChannel;
+
+    public String sendMessage(String name) {
+        GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(serverChannel);
+        GreeterOuterClass.HelloReply response = stub.sayHello(GreeterOuterClass.HelloRequest.newBuilder().setName(name).build());
+        return response.getMessage();
+    }
+}
